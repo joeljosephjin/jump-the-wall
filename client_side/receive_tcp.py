8ec7c205215905c2484db76ea06821dc4b3c2adf
Joel Joseph <joeljosephjin@gmail.com>
2023-08-17 12:42:42 +0000

    removed unnecessary files

--- client_side/receive_tcp.py
+++
@@ -1,9 +0,0 @@
-from utils_tcp import receive_file
-
-
-if __name__=="__main__":
-    # Usage example
-    file_path = 'file.txt'
-    host = '127.0.0.1'  # Server IP address
-    port = 12345  # Server port number
-    receive_file(file_path, host, port)
